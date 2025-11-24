1cd296ac5a918f0b5358c9a14ca57753ec50aabe
Safarmurod999 <supermen0904@gmail.com>
2025-11-24 18:30:49 +0500

    feat(KAN-11): write unit tests for usecases in users module

---
+++ src/application/usecases/user/delete-user/delete-user-by-id.usecase.spec.ts
@@ -0,0 +1,28 @@
+import { DeleteUserUsecaseImpl } from './delete-user-by-id.usecase';
+import { UserRepository } from 'src/domain';
+import { DeleteUserUsecaseParams } from './types';
+
+describe('DeleteUserusecaseImpl', () => {
+  let userRepository: jest.Mocked<UserRepository>;
+  let usecase: DeleteUserUsecaseImpl;
+
+  beforeEach(() => {
+    userRepository = {
+      delete: jest.fn(),
+    } as any;
+
+    usecase = new DeleteUserUsecaseImpl(userRepository);
+  });
+
+  it('should call userRepository.delete with correct id and return response', async () => {
+    const params: DeleteUserUsecaseParams = { id: '12345' };
+    const expectedResponse = 'User deleted successfully';
+
+    userRepository.delete.mockResolvedValue(expectedResponse);
+
+    const result = await usecase.execute(params);
+
+    expect(userRepository.delete).toHaveBeenCalledWith(params.id);
+    expect(result).toBe(expectedResponse);
+  });
+});
